cc4c8f8e82b017f9d28cac700e34f251d550cefb
bcastane <benja.castaneda@gmail.com>
2016-07-04 19:32:36 -0400

    creacion usuarios

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,14 @@
 class WelcomeController < ApplicationController
   def index
   end
+
+  def thanks
+
+  	@user=User.new
+  	@user.name=params[:nombre]
+  	@user.age = params[:edad].to_i
+  	@user.mail=params[:email]
+  	@user.save
+  end
+
 end
